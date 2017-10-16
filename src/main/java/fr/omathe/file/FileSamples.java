c5c5cde3509a5973a047457db2cd85c7cb9eddcd
Olivier MATHE <omathe@free.fr>
2017-10-16 21:25:40 +0200

    File sample : getFileBytes

--- src/main/java/fr/omathe/file/FileSamples.java
+++ src/main/java/fr/omathe/file/FileSamples.java
@@ -1,5 +1,6 @@
 package fr.omathe.file;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileSamples {
-	
+
 	/**
 	 * Browses a files tree
 	 * The root file is included in the returned stream
@@ -16,8 +17,19 @@ public class FileSamples {
 	 * @throws IOException
 	 */
 	Stream<Path> browseTree(final String directory) throws IOException {
-		
+
 		return Files.walk(Paths.get(directory));
+	}
+
+	byte[] getFileBytes(final File file) {
+
+		final Path path = file.toPath();
+		byte[] content = null;
+		try {
+			content = Files.readAllBytes(path);
+		} catch (final IOException e) {
+		}
+		return content;
 	}
 
 }
